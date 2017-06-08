3f79ae863051cec9997e60956654ee1ec1adba26
Jim-Lin <acgsong.tw@yahoo.com.tw>
2017-06-08 14:04:13 +0800

    Add SqrtNewton solution

--- sqrt/README.md
+++ sqrt/README.md
@@ -4,5 +4,23 @@
 
 add `0 < value < 1` case
 
+```
+Square Root of 9.0 is: 3.0000014305114746
+Square Root of 16.0 is: 4.0
+Square Root of 10.0 is: 3.1622743606567383
+Square Root of 6.7 is: 2.5884374141693116
+Square Root of 0.3 is: 0.5477256774902344
+```
+
 ## Newton
 ### ref.
+- http://blog.punkid.org/2008/02/28/compute-the-square-root-via-newtons-iteration/
+- https://www.youtube.com/watch?v=OOyPMKSggAE
+
+```
+Square Root of 9.0 is: 3.0
+Square Root of 16.0 is: 4.0
+Square Root of 10.0 is: 3.162277660168379
+Square Root of 6.7 is: 2.588435821108957
+Square Root of 0.3 is: 0.5477225575051661
+```
---
+++ sqrt/SqrtNewton.java
@@ -0,0 +1,34 @@
+public class SqrtNewton {
+
+	public static void main(String args[]) {
+		double[] inputs = {9, 16, 10, 6.7, 0.3};
+		for (int i = 0; i < inputs.length; i++) {
+			System.out.println("Square Root of " + inputs[i] + " is: " + sqrt(inputs[i]));
+		}
+	}
+    
+	private static double sqrt(double value) {
+		if (value < 0) {
+			// throw new Exception("can not caculate!");
+			return -1;
+		}
+		
+		if (value == 0 || value == 1) {
+			return value;
+		}
+		
+		double x = value / 2;
+		int times = 100;
+		
+		while (times-- > 0) {
+			if (x * x == value) {
+				return x;
+			} else {
+				// x = x - ((x * x - value) / 2 * x);
+				x = (x + (value / x)) / 2;
+			}
+		}
+		
+		return x;
+	}
+}
