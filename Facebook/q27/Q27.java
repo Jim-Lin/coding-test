04da457a81cc097ffa7919e9d43e20e2e62e8ce2
Jim-Lin <acgsong.tw@yahoo.com.tw>
2018-02-06 15:50:41 +0800

    Add Facebook coding problem

---
+++ Facebook/q27/Q27.java
@@ -0,0 +1,34 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Stack;
+
+public class Q27 {
+
+    public static void main(String args[]) {
+        System.out.println(q27("([])[]({})"));
+        System.out.println(q27("([)]"));
+        System.out.println(q27("((()"));
+    }
+
+    private static boolean q27(String s) {
+        Map<Character, Character> map = new HashMap<Character, Character>() {
+            {
+                put(')', '(');
+                put('}', '{');
+                put(']', '[');
+            }
+        };
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.get(c) == null) {
+                stack.push(c);
+            } else if (map.get(c) != stack.pop()) {
+                return false;
+            }
+        }
+
+        return stack.empty();
+    }
+}
