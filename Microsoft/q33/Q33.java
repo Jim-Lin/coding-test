1daa1a725673e4894e1ccd5690ba810466599537
Jim-Lin <acgsong.tw@yahoo.com.tw>
2018-02-11 16:32:45 +0800

    Add Microsoft coding problem

---
+++ Microsoft/q33/Q33.java
@@ -0,0 +1,61 @@
+import java.util.List;
+import java.util.LinkedList;
+
+public class Q33 {
+
+    public static void main(String args[]) {
+        int[] nums = {2, 1, 5, 7, 2, 0, 5};
+        q33(nums);
+    }
+
+    private static void q33(int[] nums) {
+        float median = nums[0];
+        System.out.println(median);
+
+        List<Integer> list = new LinkedList<>();
+        list.add(nums[0]);
+
+        for (int i = 1; i < nums.length; i++) {
+            int num = nums[i];
+            int size = list.size();
+            if (num > median) {
+                for (int j = size / 2; j < size; j++) {
+                    if (num > list.get(j)) {
+                        continue;
+                    } else {
+                        list.add(j, num);
+
+                        break;
+                    }
+                }
+
+                if (size == list.size()) {
+                    list.add(num);
+                }
+            } else {
+                for (int j = 0; j < size / 2 + 1; j++) {
+                    if (num > list.get(j)) {
+                        continue;
+                    } else {
+                        list.add(j, num);
+
+                        break;
+                    }
+                }
+
+                if (size == list.size()) {
+                    list.add(size / 2 + 1, num);
+                }
+            }
+
+            size = list.size();
+            if (size % 2 == 0) {
+                median = (Float.valueOf(list.get(size / 2 - 1)) + Float.valueOf(list.get(size / 2))) / 2;
+            } else {
+                median = Float.valueOf(list.get(size / 2));
+            }
+
+            System.out.println(median);
+        }
+    }
+}
